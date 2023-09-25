bfdfdf7fbef0d1ece3ac1a9d1594e76d97b02ff6
Iamneo <techadmin@iamneo.ai>
2023-09-25 12:19:54 +0000

    Backup Commit

---
+++ Week2Day1_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Week2Day1_4.Managers;
+
+namespace IPLBiddingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of managers
+            
+        }
+    }
+}
